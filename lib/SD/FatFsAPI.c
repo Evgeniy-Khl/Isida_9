379b3d6e71460867cf716ef55dc0935f676156d1
Evgenij Khloponin <graviton.ua@gmail.com>
2022-09-24 17:03:03 +0300

    file replacement FatFsAPI.c

--- lib/SD/FatFsAPI.c
+++ lib/SD/FatFsAPI.c
@@ -1,3 +1,5 @@
+#include "main.h"
+#include "global.h"
 #include "FatFsAPI.h"
 #include "my.h"
 
@@ -5,7 +7,7 @@ extern char USERPath[]; /* logical drive path */
 extern char fileName[], buffile[], txt[];
 
 FATFS SDFatFs;
-FRESULT res; //результат выполнения
+//FRESULT res; //результат выполнения
 DWORD fre_clust, fre_sect, tot_sect;
 FATFS *fs;
 FIL MyFile;
@@ -15,15 +17,14 @@ union sd {uint8_t sect[512]; char buffer2[512];} buffer;
 //uint8_t result;
 extern uint8_t /*Y_txt, X_left,*/ displ_num, ds18b20_amount, checkButt, Y_bottom, /*newButt,*/ card;
 
-extern uint16_t fillScreen;
-//extern int16_t ds18b20_val[], set[], touch_x;
-//extern uint32_t UnixTime;
+extern uint16_t fillScreen, pvT[];
+extern uint32_t UnixTime;
 //extern RTC_DateTypeDef sDate;
 uint32_t bwrt;   // для возврата из функции количества реально записанных байт
 
 //-- My_LinkDriver --------------------------------------------------------------
 uint8_t My_LinkDriver(void){
- uint8_t cardOk=0, res, item;
+ uint8_t cardOk=0, item;
   FATFS_LinkDriver(&USER_Driver, USERPath);
   if(f_mount(&SDFatFs,(const char*)USERPath,0)!=FR_OK) item = 9;  // Не монтируется SD карта!
   else {
@@ -36,123 +37,61 @@ uint8_t My_LinkDriver(void){
         break;
       case FR_NO_FILE:              // (4) Could not find the file
         // если файла нет то ...
-        if(f_open(&MyFile, fileName, FA_CREATE_NEW|FA_WRITE)!=FR_OK) item = 5;  // Не могу создать файл!
-        else {// формируем первую строку...
+        item = f_open(&MyFile, fileName, FA_CREATE_NEW|FA_WRITE);  // Пытаемся создать файл!
+        if(item==FR_OK){// формируем первую строку...
           sprintf(buffile,"timeStamp;");
           for(item=0; item<ds18b20_amount; item++){sprintf(txt,"t%u;",item+1); strcat(buffile,txt);}
           for(item=0; item<MAX_SET; item++){sprintf(txt,"set%u;",item+1); strcat(buffile,txt);}
           strcat(txt,"\r\n"); strcat(buffile, txt);
           for(item=0; item<LEN_BUFF; item++){if (buffile[item]==0) break;}
-          res = f_write(&MyFile, buffile, item, (void*)&bwrt);
-          if((bwrt == 0)||(res!=FR_OK))	item = 6;         // Немогу записать заголовок!
-          else {f_close(&MyFile); cardOk = 1; item = 0;}  // Новый файл создан!
+          item = f_write(&MyFile, buffile, item, (void*)&bwrt);
+          if((bwrt == 0)||(item!=FR_OK))	cardOk = 0;       // Немогу записать заголовок!
+          else {f_close(&MyFile); cardOk = 1;}              // Новый файл создан!
         }
         break;
-      case FR_OK: cardOk = 1; item = 0; break;            // sprintf(buffTFT,"SD карта определена: 0x%02X",sdinfo.type);
-    	default: item = 7; break;
+      case FR_OK: cardOk = 1; break;        // sprintf(buffile,"SD карта определена: 0x%02X",sdinfo.type);
     }
     f_mount(NULL,(TCHAR const*)USERPath,0); // Pointer to the file system object (NULL:unmount); 0:Do not mount (delayed mount)
   }
-  if (cardOk==0) FATFS_UnLinkDriver(USERPath);
+  if(cardOk==0) FATFS_UnLinkDriver(USERPath);
   return item;
 }
 
   //write ----------------------------------------------------------------------------------------------------
-DRESULT SD_write (const char* flname){
- uint8_t i;
-  if(f_mount(&SDFatFs,(const char*)USERPath,0)!=FR_OK) {
-//    ILI9341_WriteString(X_left, Y_bottom - 22, "Не монтируется SD карта!", Font_11x18, ILI9341_YELLOW, ILI9341_RED);
-  }
-  else {
-    if(f_open(&MyFile, flname, FA_OPEN_EXISTING|FA_WRITE)!=FR_OK) {
-//      ILI9341_WriteString(X_left, Y_bottom - 22, "Немогу открыть файл!", Font_11x18, ILI9341_YELLOW, ILI9341_RED);
-      card = 0; /*newButt = 1;*/ FATFS_UnLinkDriver(USERPath);
-    }
-    else {
-      uint32_t f_size = MyFile.fsize;
-      res = f_lseek(&MyFile, f_size);
-      if (res!=FR_OK){
-//        ILI9341_WriteString(X_left, Y_bottom - 22, "Ошибка SD карты!", Font_11x18, ILI9341_YELLOW, ILI9341_RED);
-      }
-      else {
-//        sprintf(buffTFT,"%u;", UnixTime);
-        for(i=0;i<ds18b20_amount;i++) {
-//          sprintf(txt,"%.1f;", (float)ds18b20_val[i]/10);
-//          strcat(buffTFT,txt);
-        }
-        for(i=0;i<MAX_SET;i++) {
-//          if (i<2) sprintf(txt,"%.1f;", (float)set[i]/10);
-//          else sprintf(txt,"%i;", set[i]);
-//          strcat(buffTFT,txt);
-        }
-//        strcat(buffTFT,"\r\n");
-        for(i=0;i<LEN_BUFF;i++) {
-//          if (buffTFT[i]==0) break;
-        }
-//        res = f_write(&MyFile, buffTFT, i,(void*)&bwrt);
-        if((bwrt==0)||(res!=FR_OK)) {
-//          ILI9341_WriteString(X_left, Y_bottom - 22, "Немогу сделать запись!", Font_11x18, ILI9341_YELLOW, ILI9341_RED);
-        }
-      }
-      f_close(&MyFile);
-      f_mount(NULL,(TCHAR const*)USERPath,0);
-//      if (displ_num==0) ILI9341_WriteString(X_left, Y_bottom - 22, " ЗАПИСАН ", Font_11x18, ILI9341_BLACK, ILI9341_GREEN);
-    }
-  }
-  return RES_OK;
-}
+//uint8_t SD_write (const char* flname, struct eeprom *t, struct rampv *ram){
+// uint8_t i, item;
+//  item = f_mount(&SDFatFs,(const char*)USERPath,0);
+//  if(item==FR_OK){  // Монтируем SD карту!
+//    item = f_open(&MyFile, flname, FA_OPEN_EXISTING|FA_WRITE);
+//    if(item==FR_OK) {
+//      uint32_t f_size = MyFile.fsize;
+//      item = f_lseek(&MyFile, f_size);
+//      if (item==FR_OK){
+//        sprintf(buffile,"%u;", UnixTime);
+//        for(i=0;i<MAX_SET;i++){
+//          if (i<2) sprintf(txt,"%.1f;", (float)ram->pvT[i]/10);
+//          else sprintf(txt,"%f;", (float)ram->pvRH/10);
+//          strcat(buffile,txt);
+//        }
+//        for(i=0;i<MAX_SET;i++) {
+//          if (i<2) sprintf(txt,"%.1f;", (float)t->spT[i]/10);
+//          else sprintf(txt,"%f;", (float)t->spRH[1]/10);
+//          strcat(buffile,txt);
+//        }
+//        strcat(buffile,"\r\n");
+//        for(i=0;i<LEN_BUFF;i++) {
+//          if (buffile[i]==0) break;
+//        }
+//        item = f_write(&MyFile, buffile, i,(void*)&bwrt);
+//        if((bwrt==0)||(item!=FR_OK)) {card = 0; FATFS_UnLinkDriver(USERPath);}
+//      }
+//      else {card = 0; FATFS_UnLinkDriver(USERPath);}
+//      f_close(&MyFile);
+//      f_mount(NULL,(TCHAR const*)USERPath,0);
+////      if (displ_num==0) ILI9341_WriteString(X_left, Y_bottom - 22, " ЗАПИСАН ", Font_11x18, ILI9341_BLACK, ILI9341_GREEN);
+//    }
+//    else {card = 0; FATFS_UnLinkDriver(USERPath);}
+//  }
+//  return item;
+//}
 
-  //read dir -------------------------------------------------------------------------------------------------
-DRESULT SD_dir (void){
-  uint8_t item;
-//  sprintf(buffTFT,"Файл  : ");
-//  strcat(buffTFT,fileName);
-//  ILI9341_WriteString(X_left, Y_txt, buffTFT, Font_11x18, ILI9341_WHITE, fillScreen);
-//  Y_txt = Y_txt+18+5;
-//	sprintf(buffTFT,"Размер: %u byte",MyFile.fsize);
-//  ILI9341_WriteString(X_left, Y_txt, buffTFT, Font_11x18, ILI9341_WHITE, fillScreen);
-//  Y_txt = Y_txt+18+5;
-//  sprintf(buffTFT,"Всего : %u строк.",allRecods);
-//  ILI9341_WriteString(X_left, Y_txt, buffTFT, Font_11x18, ILI9341_WHITE, fillScreen);
-//  Y_txt = Y_txt+18+5;
-  if (card){
-    if(f_mount(&SDFatFs,(TCHAR const*)USERPath,0)!=FR_OK) {
-      Error_Handler();
-    }
-    else {
-      fileInfo.lfname = (char*)buffer.sect;
-      fileInfo.lfsize = sizeof(buffer.sect);
-      res = f_opendir(&dir, "/");
-      if (res == FR_OK) {
-        item = 0;
-        while(1) {
-          res = f_readdir(&dir, &fileInfo);
-          if (res==FR_OK && fileInfo.fname[0]) {
-            char* fn = fileInfo.lfname;
-            if(strlen(fn)) {
-//              ILI9341_WriteString(X_left, Y_txt, fn, Font_7x10, ILI9341_WHITE, fillScreen);
-              item++;
-            }
-            else {
-//              ILI9341_WriteString(X_left, Y_txt, fileInfo.fname, Font_7x10, ILI9341_WHITE, fillScreen);
-              item++;
-              if(fileInfo.fattrib&AM_DIR) {
-//                ILI9341_WriteString(X_left, Y_txt, " [DIR]  ", Font_7x10, ILI9341_MAGENTA, fillScreen);
-                item++;
-              }
-            }
-          }
-          else break;
-          if (item==2||item==5||item==8||item==11||item==14||item==17||item==20){
-//            Y_txt = Y_txt+12;
-//            X_left = 5;
-          }
-//          else X_left = X_left + 105;
-        }
-        f_closedir(&dir);
-      }
-    }
-  }
-  return RES_OK;
-}
-  //----------------------------------------------------------------------------------------------------------
